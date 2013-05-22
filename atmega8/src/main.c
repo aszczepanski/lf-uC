d967eb195d820f21b9862fbf473b0dd19f1bc124
Tomasz Pewiński <pewniak747@gmail.com>
2013-05-22 16:54:42 +0200

    cleanups

--- atmega8/src/main.c
+++ atmega8/src/main.c
@@ -221,26 +221,6 @@ int main(void) {
         }
       }
     }
-		if (running) {
-      /*
-			tsop ^= 1;
-			if (tsop) PORTB |= (1<<PB0);
-			else PORTB &= !(1<<PB0);
-
-			counter<<=1;
-			if (counter == (1<<4)) counter=1;
-			PORTC &= !(1<<PC0);
-			PORTC &= !(1<<PC1);
-			PORTD &= !(1<<PD6);
-			PORTB &= !(1<<PB2);
-			if (counter&1) PORTC |= (1<<PC0);
-			else if (counter&(1<<1)) PORTC |= (1<<PC1);
-			else if (counter&(1<<2)) PORTB |= (1<<PB2);
-			else PORTD |= (1<<PD6);
-
-			_delay_ms(500);
-      */
-		}
 	}
 
 	return 0;
